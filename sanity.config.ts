27deb8648ec4d1918b1f947c63dce8ed547f4d11
Justine Hornales <jchornales.dev@gmail.com>
2024-09-19 08:11:13 +0800

    feat(Connect Astro and Sanity): :sparkles: Used sanity client to fetch data from cms to astro client side
    run "npm run generate:schema" to generate type from sanity schema

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,11 +1,19 @@
 // ./sanity.config.ts
 import { defineConfig } from "sanity";
 import { structureTool } from "sanity/structure";
+import { visionTool } from "@sanity/vision";
 import { schema } from "./src/sanity/schemaTypes";
 
 export default defineConfig({
   projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
   dataset: import.meta.env.PUBLIC_SANITY_DATASET,
-  plugins: [structureTool()],
+  plugins: [
+    structureTool(),
+    visionTool({
+      defaultApiVersion: "v2021-10-21",
+      defaultDataset: "production",
+    }),
+  ],
+
   schema: schema,
 });
